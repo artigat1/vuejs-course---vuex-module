89c92b6df2495c580fa7e979211e7321c4e3b72c
Steve Doggett <artigat1@gmail.com>
2020-01-26 12:06:01 +0000

    Using getters

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,12 +1,22 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+    <div>
+        <p>Counter is: {{ doubleCounter }}</p>
+        <p>Number of clicks: {{ stringCounter }} {{ ourOwn }}</p>
+    </div>
 </template>
 
 <script>
+    import { mapGetters }from 'vuex';
+    
     export default {
         computed: {
-            counter() {
-                return this.$store.state.counter;
+            ...mapGetters([
+                           'doubleCounter',
+                           'stringCounter'
+                       ]),
+            ourOwn() {
+                console.log('something');
+                return '';
             }
         }
     }
--- src/store/store.js
+++ src/store/store.js
@@ -6,5 +6,14 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
     state: {
         counter: 0
+    },
+    
+    getters: {
+        doubleCounter: state =>  {
+            return state.counter * 2;
+        },
+        stringCounter: state => {
+            return `${state.counter} clicks`;
+        }
     }
 });
